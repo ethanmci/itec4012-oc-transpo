d856e95017197e00b53937e66893a6dd1f4d2b8c
Ethan McIntyre <EthanMcIntyre@cmail.carleton.ca>
2022-10-12 19:27:45 -0400

    project setup

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'prefer-arrow-functions',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json'],
+  },
+  plugins: ['react'],
+  rules: {
+    eqeqeq: 'error',
+    semi: 'off',
+    'comma-dangle': 'always',
+    '@typescript-eslint/semi': 'off',
+    'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
+    'prefer-arrow-functions/prefer-arrow-functions': [
+      'warn',
+      {
+        classPropertiesAllowed: false,
+        disallowPrototype: false,
+        returnStyle: 'unchanged',
+        singleReturnOnly: false,
+      },
+    ],
+  },
+};
