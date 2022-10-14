2cca5491144accdbdae4fb1e175bd8719b97a29b
Ethan McIntyre <EthanMcIntyre@cmail.carleton.ca>
2022-10-14 11:15:17 -0400

    test axios api call

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import './Layout.css';
 
 // defining the component props
@@ -8,6 +9,22 @@ interface Props {
 
 // this component wraps everything, we can put headers or footers here
 const Layout: React.FC<Props> = ({ children }) => {
+  // const [stopData, setStopData] = useState(null);
+
+  // getting a stop summary (temp just so make sure it works)
+  useEffect(() => {
+    axios.get('https://api.octranspo1.com/v2.0/GetRouteSummaryForStop', {
+      params: {
+        appID: 'bc379ec3',
+        apiKey: process.env.OC_API_KEY,
+        stopNo: '3030'
+      }
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => { console.log(err); })
+  }, [])
   return (
     <div className="Layout">
       { children }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Layout from './components/layout/Layout';
+import Layout from './components/Layout/Layout';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
