016763ba3844741a09f902573827af7c92729b88
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2022-10-20 07:11:52 -0400

    changes for technical reqs

---
+++ src/components/BusCard.ts
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// defining the component props
+interface Props {
+  busName: string
+  color: string
+  textColor: string
+}
+
+// this makes using this function MUCH cleaner
+const e = React.createElement;
+
+const Layout: React.FC<Props> = ({ busName, color, textColor }) => {
+  return e('div', {
+    className: 'p-5 inline-block m-1 rounded-md md:w-1/12 w-1/3 h-14 shadow-sm relative',
+    style: { backgroundColor: `#${color}`, color: `#${textColor}` }
+  }, e('p', { className: 'text-center text-' }, busName));
+}
+
+export default Layout;
