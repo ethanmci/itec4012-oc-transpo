8c13cecccce8d16ac67680d6109689440149f60e
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2022-11-18 18:38:50 -0500

    i lied :)

--- src/components/HeaderTile.ts
+++ src/components/HeaderTile.ts
@@ -12,8 +12,8 @@ const e = React.createElement;
 
 const HeaderTile: React.FC<Props> = ({ title, link }) => {
   return e('div', {
-    className: 'p-5 rounded-md hover:border-2 h-14 shadow-sm relative'
-  }, e('a', { className: 'text-center text-xl', href: link }, title));
+    className: 'inline-block w-32 p-5 rounded-md hover:bg-slate-200 h-14 shadow-sm relative'
+  }, e('a', { className: 'text-center text', href: link }, title));
 }
 
 export default HeaderTile;
