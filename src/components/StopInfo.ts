374d7542069bcb024bcb4a23d64e8e2516352c50
redchamelon9 <48802350+redchamelon9@users.noreply.github.com>
2022-12-04 22:03:42 -0500

    Working on querying stops + routes
    - Getting routes based on stop info

---
+++ src/components/StopInfo.ts
@@ -0,0 +1,23 @@
+import React from 'react';
+// import BusInfoCard from './BusInfoCard';
+
+// defining the component props
+interface Props {
+  stopName: string
+  color: string
+  textColor: string
+}
+
+// this makes using this function MUCH cleaner
+const e = React.createElement;
+
+const StopInfo: React.FC<Props> = ({ stopName, color, textColor }) => {
+  return e('div', {
+    onClick: () => console.log(`Click handled for: ${stopName}!`),
+    className: 'p-5 rounded-sm hover:border-2 h-14 shadow-sm relative transition-opacity duration-75',
+    style: { backgroundColor: `#${color}`, color: `#${textColor}` },
+  }, e('p', { className: 'text-center text-xl' }, stopName),
+     e('p', { className: 'text-center text-xl' }));
+}
+
+export default StopInfo;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import StopInfo from '../components/StopInfo'
 
 interface Stop {
   id: string
@@ -21,32 +22,99 @@ interface GtfsStopQuery {
   Gtfs?: Stop[]
 }
 
+interface Route {
+  id: String
+  route_id: String
+  route_short_name: String
+  route_long_name: String
+  route_desc: String
+  route_type: String
+}
+
+interface GtfsRouteQuery {
+  Query?: object
+  Gtfs?: Route[]
+}
+
+interface inboundTrip {
+  StopNo: string
+  StopLabel: string
+  error: string
+  RouteDirection_RouteNo: string
+  RouteDirection_RouteLabel: string
+  RouteDirection_Direction: string
+  RouteDirection_Error: string
+  RRouteDirection_RequestProcessingTime: string
+  Trip_Longitude: string
+  Trip_Latitude: string
+  Trip_GPSSpeed: string
+  Trip_TripDestination: string
+  Trip_TripStartTime: string
+  Trip_AdjustedScheduleTime: string
+  Trip_AdjustmentAge: string
+  Trip_LastTripOfSchedule: string
+  Trip_BusType: string
+}
+
+interface GtfsTripQuery {
+  Query?: object
+  Gtfs?: inboundTrip[]
+}
+
 const Home: React.FC = () => {
   const [location, setLocation] = useState<google.maps.LatLngLiteral>({ lat: 0, lng: 0 });
-  const [stopList, setStopList] = useState<GtfsStopQuery>({})
+  const [stopList, setStopList] = useState<GtfsStopQuery>({});
+  const [filteredStopList, setFilteredStopList] = useState<GtfsStopQuery>({});
+  const [routeList, setRouteList] = useState<GtfsRouteQuery>({});
+  const [filteredRouteList, setFilteredRouteList] = useState<GtfsRouteQuery>({});
+  const [inboundTrip, setInbountTrip] = useState<GtfsTripQuery>({});
 
   useEffect(() => {
     if (Object.keys(stopList).length !== 0) return
 
-    void fetch(`${
-      (process.env.NODE_ENV ?? 'development') === 'development'
-        ? 'https://api.allorigins.win/get?url='
-        : ''
-    }
+    void fetch(`${(process.env.NODE_ENV ?? 'development') === 'development'
+      ? 'https://api.allorigins.win/get?url='
+      : ''
+      }
     ${encodeURIComponent(
-      `https://api.octranspo1.com/v2.0/Gtfs?${new URLSearchParams({
-        appID: process.env.REACT_APP_OC_APP_ID ?? 'KEY_NOT_FOUND',
-        apiKey: process.env.REACT_APP_OC_API_KEY ?? 'KEY_NOT_FOUND',
-        table: 'stops',
-        format: 'json',
-      }).toString()}`,
-    )}`)
+        `https://api.octranspo1.com/v2.0/Gtfs?${new URLSearchParams({
+          appID: process.env.REACT_APP_OC_APP_ID ?? 'KEY_NOT_FOUND',
+          apiKey: process.env.REACT_APP_OC_API_KEY ?? 'KEY_NOT_FOUND',
+          table: 'stops',
+          format: 'json',
+        }).toString()}`,
+      )}`)
       .then(async (response) => {
         if (response.ok) return await response.json()
         throw new Error('Network response was not ok.')
       })
       .then((data) => {
         setStopList(JSON.parse(data.contents))
+      })
+  }, [])
+
+  // For routes
+  useEffect(() => {
+    if (Object.keys(routeList).length !== 0) return
+
+    void fetch(`${(process.env.NODE_ENV ?? 'development') === 'development'
+      ? 'https://api.allorigins.win/get?url='
+      : ''
+      }
+    ${encodeURIComponent(
+        `https://api.octranspo1.com/v2.0/Gtfs?${new URLSearchParams({
+          appID: process.env.REACT_APP_OC_APP_ID ?? 'KEY_NOT_FOUND',
+          apiKey: process.env.REACT_APP_OC_API_KEY ?? 'KEY_NOT_FOUND',
+          table: 'routes',
+          format: 'json',
+        }).toString()}`,
+      )}`)
+      .then(async (response) => {
+        if (response.ok) return await response.json()
+        throw new Error('Network response was not ok.')
+      })
+      .then((data) => {
+        setRouteList(JSON.parse(data.contents))
       })
   }, [])
 
@@ -59,22 +127,52 @@ const Home: React.FC = () => {
   const withinRange = (curLat: number, lat: number, curLon: number, lon: number): boolean => {
     const radius: number = 0.00225225
     if (lat < (curLat + radius) && lat > (curLat - radius) &&
-        lon < (curLon + radius) && lon > (curLon - radius)) return true;
+      lon < (curLon + radius) && lon > (curLon - radius)) return true;
     return false;
   }
 
-  const simpleStopDisplay = (): void => {
-    return (
-      stopList.Gtfs?.forEach((e) => {
-        const lat: number = parseFloat(e.stop_lat)
-        const lon: number = parseFloat(e.stop_lon)
-
-        if (withinRange(location.lat, lat, location.lng, lon)) {
-          console.log(e.stop_name, e.stop_lat, e.stop_lon)
-        }
-      })
-    )
+  const getStopsNearby = (): void => {
+    const returnArr: Stop[] = []
+    stopList.Gtfs?.forEach((e) => {
+      const lat: number = parseFloat(e.stop_lat)
+      const lon: number = parseFloat(e.stop_lon)
+      if (withinRange(location.lat, lat, location.lng, lon)) {
+        returnArr.push(e)
+        getRoutes()
+      }
+    })
+    setFilteredStopList({ ...setFilteredStopList, Gtfs: [...returnArr] })
   }
+
+  const getRoutes = (): void => {
+    const returnArr: Route[] = []
+    routeList.Gtfs?.forEach((e) => {
+      returnArr.push(e)
+    })
+    setFilteredRouteList({ ...setFilteredRouteList, Gtfs: [...returnArr] })
+  }
+
+  const stopDisplayTemp = filteredStopList.Gtfs?.map((item, index) => {
+    return (
+      <StopInfo
+        key={index}
+        stopName={item.stop_name}
+        color={'00000'}
+        textColor={'black'}
+      ></StopInfo>
+    )
+  })
+
+  const routesDisplayTemp = filteredRouteList.Gtfs?.map((item, index) => {
+    return (
+      <StopInfo
+        key={index}
+        stopName={item.stop_name}
+        color={'00000'}
+        textColor={'black'}
+      ></StopInfo>
+    )
+  })
 
   const simpleLocationDisplay = (): void => {
     return (
@@ -84,9 +182,12 @@ const Home: React.FC = () => {
 
   return (
     <div>Home page!<br></br>
-      <button onClick={simpleStopDisplay}>Stops in range</button>
+      <button onClick={getStopsNearby}>Stops in range</button>
       <br></br>
       <button onClick={simpleLocationDisplay}>Current Lat</button>
+      <div className='inline-block mx-auto'>List of Stops:
+        <div className='border-2 border-red-500 rounded-lg align-center'>{stopDisplayTemp}</div>
+      </div>
     </div>
   )
 };
