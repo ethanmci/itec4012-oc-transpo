8ce810cda642a16298f96aa187da64fb726c0b5b
KB <60189784+benfowlercool@users.noreply.github.com>
2022-12-09 21:31:51 -0500

    header
    the rest of it

--- src/components/HeaderTile.ts
+++ src/components/HeaderTile.ts
@@ -12,7 +12,7 @@ const e = React.createElement;
 
 const HeaderTile: React.FC<Props> = ({ title, link }) => {
   return e('div', {
-    className: 'flex justify-center bg-gray-900 text-white',
+    className: 'flex justify-center bg-gray-900 text-white text-2xl',
   }, e('a', { className: 'text-center text', href: link }, title));
 }
 
